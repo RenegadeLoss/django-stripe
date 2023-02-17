c451bcc89f99595f626823755fe7039e112b86be
RenegadeLoss <renegadeloss696@gmail.com>
2023-02-17 15:39:14 +0300

    ...

--- test_stripe/build.sh
+++ test_stripe/build.sh
@@ -1,9 +1,3 @@
 set -o errexit
 
-pip install poetry
-poetry install
-
-cd /opt/render/project/src/test_stripe/src
-python manage.py collectstatic --no-input
-python manage.py makemigrations
-python manage.py migrate
+docker-compose up
